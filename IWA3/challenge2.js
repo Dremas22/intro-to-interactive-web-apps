2303df7a2e6069895699dd8b624981e5f2a58d88
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-03-28 00:08:22 +0200

    changes

---
+++ IWA3/challenge2.js
@@ -0,0 +1,26 @@
+// nwabisa.js
+
+/*private firstname = "Nwabisa"
+private surname = "Gabe"
+public role = "CEO"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#nwabisa').innerText = display
+ 
+
+ 
+
+// johannes.js
+
+private firstname = "Johannes"
+private surname = "Potgieter"
+public role = "Intern"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display */
+
+ 
+
+// scripts.js
+
+console.log('Roles:', nwabisa.role, johanes.role, alex.role)
