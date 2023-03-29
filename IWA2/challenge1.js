1aa0207d748a13707db68013e582039712d2175f
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-03-29 09:03:10 +0200

    changes

--- IWA2/challenge1.js
+++ IWA2/challenge1.js
@@ -1,3 +1,13 @@
+/* This is the firstname of the user */
+
+//const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'*/
+
+
+//console.log(user; surname;)
+
 
 const user = 'John';
 const surname = 'Smith';
