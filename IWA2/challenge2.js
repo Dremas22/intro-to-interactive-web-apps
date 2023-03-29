f2bc257be20f4be3cfa2945f604863a36f9b6be8
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-03-29 13:24:07 +0200

    changes

--- IWA2/challenge2.js
+++ IWA2/challenge2.js
@@ -1,8 +1,6 @@
 /** This is the date that a user created their account */
 
-/*
- * This is the date that a user created their account 
- */
+
 const date = '10/07/2014';
 
 console.log(date);
---
+++ IWA4/challenge1/script4.js
@@ -0,0 +1,39 @@
+let count = 0;
+
+if (date = 2050) {
+
+    const date = 2050;
+    let status = "";
+
+	let monthFour = 'April'; //assign a local variable for April
+	console.log(monthFour, 'Family Day');
+	console.log(monthFour, 'Freedom Day');
+	count = count + 2; //remove let because count variable is already been declared with a "const"
+
+	if (status = "student") {
+	   console.log('June', 'Youth Day')
+	   count += 1;
+    } //indent code block()
+
+    if (status = "parent") {
+        console.log(date, 'Christmas Day')
+        count += 1;
+    }
+
+    console.log("January", "New Year’s Day")
+    console.log("March", 'Human Rights Day')
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+    count += 4;
+
+	let monthTwelve = 'December';
+	console.log(monthTwelve, 'Day of Reconciliation');
+	count += 1;
+
+	console.log(date, 'Day of Goodwill')
+	count += 1;
+}
+
+console.log('Your status is:', "Parent or Student")
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
