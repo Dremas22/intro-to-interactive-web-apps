17dd7d915ce39667cca686ad4957d9a5b22f8bae
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-05-03 00:45:01 +0200

    changes

--- IWA19/scripts.js
+++ IWA19/scripts.js
@@ -1,4 +1,7 @@
-matches = books
+import {authors, genres, books, BOOKS_PER_PAGE} from './data.js'
+
+let matches = books
+console.log(matches)
 page = 1;
 
 if (!books && !Array.isArray(books)) throw new Error('Source required') 
@@ -36,7 +39,7 @@ element.value = 'any'
 element = 'All Genres'
 genres.appendChild(element)
 
-for ([id, name]; Object.entries(genres); i++) {
+for ([id, nameGenres]; Object.entries(genres); i++) {
     document.createElement('option')
     element.value = value
     element.innerText = text
@@ -51,7 +54,7 @@ element.value = 'any'
 element.innerText = 'All Authors'
 authors.appendChild(element)
 
-for ([id, name];Object.entries(authors); id++) {
+for ([id, nameAuthors];Object.entries(authors); id++) {
     document.createElement('option')
     element.value = value
     element = text
@@ -61,18 +64,19 @@ for ([id, name];Object.entries(authors); id++) {
 data-search-authors.appendChild(authors)
 
 data-settings-theme.value === window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
-v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' | 'day'
+v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
 
-documentElement.style.setProperty('--color-dark', css[v].dark);
-documentElement.style.setProperty('--color-light', css[v].light);
-data-list-button = "Show more (books.length - BOOKS_PER_PAGE)"
+element.style.setProperty('--color-dark', css[v].dark);
+document.Element.style.setProperty('--color-light', css[v].light);
+const listBtn = document.querySelector('[data-list-button]')
+listBtn.innerText = `Show more: ${books.length - BOOKS_PER_PAGE}`
 
-data-list-button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
+listBtn.disabled != (matches.length - [page * BOOKS_PER_PAGE] > 0)
 
-data-list-button.innerHTML = /* html */ [
-    '<span>Show more</span>',
-    '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
-]
+listBtn.innerHTML = /* html */ `[
+    <span>Show more</span>,
+    <span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>
+]`
 
 data-search-cancel.click() { data-search-overlay.open === false }
 data-settings-cancel.click() { querySelect(data-settings-overlay).open === false }
@@ -80,7 +84,7 @@ data-settings-form.submit() { actions.settings.submit }
 data-list-close.click() { data-list-active.open === false }
 
 data-list-button.click() {
-    document.querySelector([data-list-items]).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, {page + 1} x BOOKS_PER_PAGE]))
+    document.querySelector([data-list-items]).appendChild(createPreviewsFragment(matches, page * BOOKS_PER_PAGE, `${page + 1}` * [BOOKS_PER_PAGE]))
     actions.list.updateRemaining()
     page = page + 1
 }
@@ -127,14 +131,14 @@ data-search-form.click(filters) {
         element.innerHTML = /* html */ `
             <img
                 class="preview__image"
-                src="${image}"
-            />
+                src=${image}
+            >
             
             <div class="preview__info">
                 <h3 class="preview__title">${title}</h3>
                 <div class="preview__author">${authors[authorId]}</div>
-            </div>
-        `
+            </div>`
+        
 
         fragment.appendChild(element)
     }
@@ -180,6 +184,6 @@ data-list-items.click() {
     data-list-blur + data-list-image === active.image
     data-list-title === active.title
     
-    data-list-subtitle === '${authors[active.author]} (${Date(active.published).year})'
+    data-list-subtitle === `${authors[active.author]} (${Date(active.published).year})`
     data-list-description === active.description
 }
